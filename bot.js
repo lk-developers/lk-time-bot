33fd505dbf4ad48a20a8e3a04181aecef1eae107
Sandaru Fernando <46101449+dexterlk@users.noreply.github.com>
2019-08-05 21:12:26 +0530

    Fix: typo in bot.js

--- bot.js
+++ bot.js
@@ -79,7 +79,7 @@ client.on("guildDelete", (guild) => {
 });
 
 const updateActivity = () => {
-    client.user.setActivity(`Serving ${client.guilds.size} servers | By LKDevelopers 🇱🇰`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers | By LK Developers 🇱🇰`);
 };
 
 // login to client
